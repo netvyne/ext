655e6ed111fc2a2cc1c68407fa803199f453303f
Ali Raza <ali@livesports.ai>
2021-06-28 09:43:39 +0500

    env files changes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,57 +1,6 @@
-const path = require("path");
-const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-const getClientEnvironment = require("./config/env");
-const paths = require("./config/paths");
-
-// Webpack uses `publicPath` to determine where the app is being served from.
-// In development, we always serve from the root. This makes config easier.
-const publicPath = "/";
-// `publicUrl` is just like `publicPath`, but we will provide it to our app
-// as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
-// Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-const publicUrl = "";
-// Get environment variables to inject into our app.
-const env = getClientEnvironment(publicUrl);
-
-module.exports = {
-  entry: {
-    backgroundPage: path.join(__dirname, "src/backgroundPage.ts"),
-    popup: path.join(__dirname, "src/popup/index.tsx"),
-  },
-  output: {
-    path: path.join(__dirname, "dist/js"),
-    filename: "[name].js",
-  },
-  module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        test: /\.tsx?$/,
-        use: "ts-loader",
-      },
-      {
-        exclude: /node_modules/,
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "style-loader", // Creates style nodes from JS strings
-          },
-          {
-            loader: "css-loader", // Translates CSS into CommonJS
-          },
-          {
-            loader: "sass-loader", // Compiles Sass to CSS
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    alias: {
-      "@src": path.resolve(__dirname, "src/"),
-    },
-  },
-  plugins: [new webpack.DefinePlugin(env.stringified)],
-};
+module.exports = merge(common, {
+    mode: "production",
+});
