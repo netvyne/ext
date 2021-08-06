9eae0718ce13e95741f4a43209e510f78519a57c
khalilsarwari <khalil.sarwari@berkeley.edu>
2021-08-05 17:45:01 -0700

    add build

--- webpack.common.js
+++ webpack.common.js
@@ -1,56 +1,56 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
-const getClientEnvironment = require("./config/env");
-const paths = require("./config/paths");
+const getClientEnvironment = require('./config/env');
+const paths = require('./config/paths');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
-const publicPath = "/";
+const publicPath = '/';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-const publicUrl = "";
+const publicUrl = '';
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
 
 module.exports = {
   entry: {
-    backgroundPage: path.join(__dirname, "src/backgroundPage.ts"),
-    popup: path.join(__dirname, "src/popup/index.tsx"),
+    backgroundPage: path.join(__dirname, 'src/backgroundPage.ts'),
+    popup: path.join(__dirname, 'src/popup/index.tsx'),
   },
   output: {
-    path: path.join(__dirname, "dist/js"),
-    filename: "[name].js",
+    path: path.join(__dirname, 'extension/js'),
+    filename: '[name].js',
   },
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
       },
       {
         exclude: /node_modules/,
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader", // Creates style nodes from JS strings
+            loader: 'style-loader', // Creates style nodes from JS strings
           },
           {
-            loader: "css-loader", // Translates CSS into CommonJS
+            loader: 'css-loader', // Translates CSS into CommonJS
           },
           {
-            loader: "sass-loader", // Compiles Sass to CSS
+            loader: 'sass-loader', // Compiles Sass to CSS
           },
         ],
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
     alias: {
-      "@src": path.resolve(__dirname, "src/"),
+      '@src': path.resolve(__dirname, 'src/'),
     },
   },
   plugins: [new webpack.DefinePlugin(env.stringified)],
